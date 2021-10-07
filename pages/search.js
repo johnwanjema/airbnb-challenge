cc7336c3655bc16b2b0d395f04ceb4bda8ba8c7e
wanjema <jonwanjema@gmail.com>
2021-10-07 21:08:56 +0300

    add search component

---
+++ pages/search.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+function Search() {
+    return (
+        <div>
+            <Header />
+            <main className='flex'>
+                <section className='flex-grow pt-14 px-6'>
+                    <p className='text-xs'>300+ Stays - 07 October 21 - 07 October 21 - for 1 guests</p>
+                    <h1 className='text-3xl text-semi-bold mt-2 mb-6'>Stays in london</h1>
+                    <div className='hidden lg:inline-flex border-b mb-5 space-x-3 text-gray-800  whitespace-nowrap'>
+                        <p className='button'>Cancellation flexibility</p>
+                        <p className='button'>Type of place</p>
+                        <p className='button'>Price</p>
+                        <p className='button'>Rooms and beds</p>
+                        <p className='button'>More filters</p>
+                    </div>
+                </section>
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default Search
